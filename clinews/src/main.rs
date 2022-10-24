58dc0c54a74563e87906a5832a781b1601dcb480
celeri <celestin.bouchet@gmail.com>
2022-10-24 16:33:08 +0200

    git stuff

--- clinews/src/main.rs
+++ clinews/src/main.rs
@@ -1,14 +1,12 @@
 mod theme;
 
-use std::error::Error;
 use newsapi::{Article, Country, Endpoint, NewsAPI};
+use std::error::Error;
 
-fn render_articles(articles: &Vec<Article>)
-{
+fn render_articles(articles: &Vec<Article>) {
     let theme = theme::default();
     theme.print_text("# Top headlines\n\n");
-    for article in articles
-    {
+    for article in articles {
         theme.print_text(&format!("`{}`", article.title()));
         theme.print_text(&format!("> *{}*", article.url()));
         theme.print_text(article.description().unwrap_or(&"".to_string()));
@@ -16,8 +14,7 @@ fn render_articles(articles: &Vec<Article>)
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>>
-{
+fn main() -> Result<(), Box<dyn Error>> {
     let api_key = std::env::var("API_KEY")?;
 
     let mut newsapi = NewsAPI::new(&api_key);
